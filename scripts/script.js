93c3f17bf854d48f5be0f5f9f3cc36a633a8945f
A1ejand7omora <luisalejandromorantes836@gmail.com>
2021-11-17 10:21:41 -0500

    Merge branch 'developers'

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+let nombrePlanetas=["Tierra","Marte"]
+let distancia = [6, 3, "67"]
+const tamanio =[]
+
+let i=0;
+while (i<= nombrePlanetas.length - 1) {
+    console.log(i)
+    console.log(nombrePlanetas[i]);
+    i=i+1
+}
+
+for(const key in distancia){
+    if (distancia[key] == "5") {
+        console.log("Se Encontro La Distancia")
+    } else {
+        console.log("No Se Encontro La Distancia")
+        
+    }
+}   
